488a492c17b2e594e29795cd4febd9c23201b729
Marcus Oliveira <marcus.oliveira2403@gmail.com>
2023-03-20 15:09:44 -0300

    atualização 20/03

--- lista_imagem/App.js
+++ lista_imagem/App.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, FlatList, Image,  Text, View } from 'react-native';
 
+function Card(props) {
+
+  return (
+    <View style={styles.card}>
+      <Image
+        style={{
+          resizeMode:'100%',
+          width: '100%',
+          height: 200,
+          marginBottom: 10
+        }}/>
+    </View>
+  )
+}
 export default function App() {
+  const livro = require('./assets/images/nada.jpeg')
+
   return (
     <View style={styles.container}>
-      <Text>Livraria</Text>
-      <View>        
-        <Text style={{ fontSize: 20, color: '#fff'}}>Livro 1</Text>
-      </View>
-      <View>        
-        <Text style={{ fontSize: 20, color: '#fff'}}>Livro 1</Text>
-      </View>
-      <View>        
-        <Text style={{ fontSize: 20, color: '#fff'}}>Livro 1</Text>
-      </View>
-      <View>        
-        <Text style={{ fontSize: 20, color: '#fff'}}>Livro 1</Text>
+      <Text style={{ fontSize: 30, marginBottom: 20 }}>Livraria</Text>
+      <View style={styles.card}>
+        <Image style={{resizeMode: 'stretch', width: '100%', height: '200', marginBottom: 10 }} source={livro} />
+        <Text style={{ fontSize: 20, color: '#fff'}}>Título</Text>
+        <Text style={{ fontSize: 20, color: '#fff'}}>Preço</Text>
       </View>
     </View>
   );
@@ -27,5 +36,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    backgroundColor: '#555',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  card: {
+    backgroundColor: '#000',
+    width: '40%',
+    height: 270,
+    borderRadius: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
   },
 });
